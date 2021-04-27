0573135a363bc7e2b1c6b9657d3715b665d39548
Vu Vuong <vu.vuong@eastgate-software.com>
2021-04-27 16:13:38 +0700

    remove exception

--- plugin/FormPost.php
+++ plugin/FormPost.php
@@ -131,7 +131,6 @@ abstract class FormPost
 			$this->analyseFormContentByPrefix($pFormData);
 			$pFormData->setStatus(self::MESSAGE_SUCCESS);
 		} catch (Exception $pException) {
-			var_dump($pException->getTrace());
 			$pFormData->setStatus(self::MESSAGE_ERROR);
 			$this->_pFormPostConfiguration->getLogger()->logError($pException);
 		}
